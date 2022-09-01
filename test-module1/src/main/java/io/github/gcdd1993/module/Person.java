f135e99910788a3869d1b252343ebf4a618fd94c
Musk.Chen <Musk.Chen@fanruan.com>
2022-09-01 19:00:35 +0800

    first commit

---
+++ test-module1/src/main/java/io/github/gcdd1993/module/Person.java
@@ -0,0 +1,14 @@
+package io.github.gcdd1993.module;
+
+import lombok.Data;
+
+/**
+ * @author <a href="mailto:Musk.Chen@fanruan.com">Musk.Chen</a>
+ * @version 2.1
+ * Created by Musk.Chen on 2022/9/1
+ */
+@Data
+public class Person {
+    private String name;
+    private Integer age;
+}
